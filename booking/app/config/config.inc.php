9e825326824e6a6e76a382e774b77da776a90f15
Miguel Romero <ingmromero34@gmail.com>
2016-06-06 01:10:10 -0500

    ok

--- booking/app/config/config.inc.php
+++ booking/app/config/config.inc.php
@@ -4,9 +4,9 @@ if (!defined("PJ_USER")) define("PJ_USER", "hbzzadmin");
 if (!defined("PJ_PASS")) define("PJ_PASS", "C@c6w217DS");
 if (!defined("PJ_DB")) define("PJ_DB", "naturabase");
 if (!defined("PJ_PREFIX")) define("PJ_PREFIX", "bk_");
-if (!defined("PJ_INSTALL_FOLDER")) define("PJ_INSTALL_FOLDER", "/produccion/booking/");
-if (!defined("PJ_INSTALL_PATH")) define("PJ_INSTALL_PATH", "C:/wamp/www/produccion/booking/");
-if (!defined("PJ_INSTALL_URL")) define("PJ_INSTALL_URL", "http://localhost/produccion/booking/");
+if (!defined("PJ_INSTALL_FOLDER")) define("PJ_INSTALL_FOLDER", "/booking/");
+if (!defined("PJ_INSTALL_PATH")) define("PJ_INSTALL_PATH", "C:/wamp/www/booking/");
+if (!defined("PJ_INSTALL_URL")) define("PJ_INSTALL_URL", "http://localhost/booking/");
 if (!defined("PJ_SALT")) define("PJ_SALT", "Z9L7N5H3");
 if (!defined("PJ_INSTALLATION")) define("PJ_INSTALLATION", "MTI3NDQzNTMzNTQxNjMzNzAxMTcwNDQ3MzYyNzI0NTI3NzkxMDI3MzA1Mzg0MTEwMTczNDQ3MjY0MTUwMDE0NzkyNTI5MDk0MzE3OTUxNjQ1NTc3NTQzMjI4MDkxNzEwMjM1MCAxMTg1ODcxMzkwOTY0MzE4Njk2MTgyMjE3NDM0OTIzODg3MjE4MTY1NjA2Njk3ODA1MzM0MjY4NzUyMzQ4MTIwODk0MzAyNjI5NzMwNDkyMjA1MTY1MTMzMTI1Njk1NzUzMDM2IDk0MjY4NDAyMTM2MDE4ODM0NTk4MzU1NDMyNzg1NjIzMjQ5MTM1MDU0ODY4NTczMzk4ODg2MDIyMjc2OTAwNDQ1OTAwMTMwMTY2MzAwMTUyNDcyMDEwMDMwNDMzNzgxMTcyNw==");
 ?>
